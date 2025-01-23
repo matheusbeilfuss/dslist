b148d2f3d59c431c41b6bb3bf93b0ad9bdbfa8e4
Beilfuss <matheusdicimb@gmail.com>
2025-01-23 09:28:42 +0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired // injeta uma instância do GameRepository dentro de GameService
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true) // garante que a transação do BD vai acontecer obedecendo os princípios ACID
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get(); // .get() para pegar o game dentro de "optional"
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList(); // transforma lista de Games em GameMinDTO
