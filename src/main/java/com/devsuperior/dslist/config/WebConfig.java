8d966aca029a843b214bdd3e8a8cbcd5e1de3441
Beilfuss <matheusdicimb@gmail.com>
2025-01-24 09:34:51 +0300

    CORS config

---
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dslist.config;
+
+@Configuration
+public class WebConfig {
+
+	@Value("${cors.origins}")
+	private String corsOrigins;
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+			}
+		};
+	}
+	
+}
